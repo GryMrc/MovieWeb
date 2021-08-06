bf0b43ada3c990169e16feb830764af2d0de2967
guray meric <guray.meric@overtech.com.tr>
2021-08-06 18:15:59 +0300

    created backend

---
+++ netcore/Movie.Backend/Movie.Backend/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MovieController : ControllerBase
+    {
+        [HttpPost]
+        public Task<IActionResult> Create()
+        {
+
+            return null;
+        }
+    }
+}
